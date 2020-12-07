2cd2fa2144c166e98fad32e354833c02c13fcba2
ky28059 <60120929+ky28059@users.noreply.github.com>
2020-12-06 19:49:41 -0800

    Preferred staff name parsing

--- client/src/components/lists/Staff.js
+++ client/src/components/lists/Staff.js
@@ -11,9 +11,18 @@ import StaffComponent from "./StaffComponent";
 const Staff = (props) => {
     const [query, setQuery] = useState('');
 
-    const lastName = (name) => {
-        let arr = name.split(' ');
-        return arr[arr.length - 1];
+    // Parses last names to find the preferred last name by matching it with the staff email
+    const preferredLastName = (staff) => {
+        let {name, email} = staff;
+
+        // Replaces dashes with spaces to prevent matching Barba-Medina to nmedina (instead individually matching barba and medina)
+        // Removes apostrophes to prevent matching O'Connell with coconnell
+        let lastNames = name.replace('-', ' ').replace('\'', '').split(' ').slice(1);
+
+        for (let lastName of lastNames) {
+            let lower = lastName.toLowerCase();
+            if (email.match(lower)) return lastName;
+        }
     }
 
     return (
@@ -45,7 +54,7 @@ const Staff = (props) => {
                         periods={staff.periods}
                     />
                 }
-                sort={([idA, staffA], [idB, staffB]) => lastName(staffA.name).localeCompare(lastName(staffB.name))}
+                sort={([idA, staffA], [idB, staffB]) => preferredLastName(staffA).localeCompare(preferredLastName(staffB))}
             />
         </>
     );
