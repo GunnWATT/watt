3e9204e521472ce79a1a8270869ba0c1a4614bfb
Roger Fan <rogerjoeyfan@gmail.com>
2021-08-08 10:02:24 -0700

    Create dropup for mobile users so they can actually sign in

--- client/src/components/layout/BottomNav.tsx
+++ client/src/components/layout/BottomNav.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {Container} from 'reactstrap';
 
 // Icons
@@ -8,14 +8,27 @@ import {
     CheckSquare,
     Users,
     Settings,
-    Tool
+    Tool,
+    ChevronUp,
+    ChevronDown
 } from 'react-feather';
 
 // Components
 import SidebarItem from './SidebarItem';
+import GoogleSignInBtn from "../auth/GoogleSignInBtn"
+import GoogleSignOutBtn from '../auth/GoogleSignOutBtn';
+
+import firebase from '../../firebase/Firebase';
+import { useAuthState } from 'react-firebase-hooks/auth';
 
 
 const BottomNav = () => {
+
+    const [showDropUp, setDropUp] = useState<boolean>(false);
+
+    const auth = firebase.auth;
+    const [user] = useAuthState(auth);
+
     return (
         <footer className="bottom-nav">
             <Container className="nav-container">
@@ -24,7 +37,28 @@ const BottomNav = () => {
                 <SidebarItem name="Classes" to="/classes" icon={<CheckSquare/>}/>
                 <SidebarItem name="Home" to="/" icon={<Home/>} exact/>
                 <SidebarItem name="Utilities" to="/utilities" icon={<Tool/>}/>
-                <SidebarItem name="Settings" to="/settings" icon={<Settings/>}/>
+
+                {/* Dropup */}
+                <div className="item dropup-wrapper">
+                    <a onClick={() => setDropUp(!showDropUp)}>
+                        {showDropUp ? <ChevronDown /> : <ChevronUp />}
+                    </a>
+
+                    <div className="bottom-nav-dropup" hidden={!showDropUp}>
+
+                        {/* Settings */}
+                        <SidebarItem name="Settings" to="/settings" icon={<Settings />} />
+                        
+                        {/* Sign In / Out */}
+                        {
+                            user ?
+                                <GoogleSignOutBtn /> :
+                                <GoogleSignInBtn />
+                        }
+                    </div>
+                </div>
+
+                {/* <SidebarItem name="Settings" to="/settings" icon={showDropUp ? <ChevronDown /> : <ChevronUp/>}/> */}
             </Container>
         </footer>
     );
