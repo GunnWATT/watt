b429dfac99f193c647097c8203b85838df8e3a06
Kevin Yu <kevinyu567@gmail.com>
2021-08-29 12:04:53 -0700

    Implement Sean's pan/rotate algorithm

---
+++ client/src/components/utilities/ImageMap.tsx
@@ -0,0 +1,220 @@
+import React, {useContext, useEffect, useRef, useState} from 'react';
+import GunnMapImage from '../../assets/gunnmap.png';
+import UserDataContext from '../../contexts/UserDataContext';
+
+
+type Pointer = {
+    id: number,
+    initX: number, initY: number,
+    lastX: number, lastY: number,
+    transformation?: Matrix,
+    other?: Pointer | null
+}
+const ImageMap = () => {
+    const userData = useContext(UserDataContext);
+
+    const mapRef = useRef<HTMLImageElement | null>(null);
+    const wrapperRef = useRef<HTMLDivElement | null>(null);
+    const [pointer, setPointer] = useState<Pointer | null>(null);
+    const [transformation, setTransformation] = useState(identity());
+
+    useEffect(() => {
+        // Unideal, but https://github.com/facebook/react/issues/14856 forces this
+        // onWheel and onTouchMove are both passive event listeners so preventDefault does not work within them
+        mapRef.current?.addEventListener('wheel', (e) => e.preventDefault());
+    }, [mapRef])
+
+    // When a pointer deactivates
+    const pointerEnd = (e: React.PointerEvent<HTMLImageElement>) => {
+        if (pointer?.id === e.pointerId) {
+            if (pointer.other) {
+                // Make the other pointer the primary pointer now
+                setPointer({
+                    ...pointer.other,
+                    initX: pointer.other.lastX,
+                    initY: pointer.other.lastY,
+                    transformation,
+                    other: null
+                })
+            } else {
+                setPointer(null);
+            }
+        } else if (pointer?.other?.id === e.pointerId) {
+            pointer.initX = pointer.lastX
+            pointer.initY = pointer.lastY
+            pointer.other = null
+            pointer.transformation = transformation
+        }
+    }
+
+    return (
+        <div
+            className="map-wrapper"
+            ref={wrapperRef}
+            onPointerDown={(e) => {
+                if (pointer) {
+                    if (!pointer.other) {
+                        setPointer({
+                            ...pointer,
+                            initX: pointer.lastX,
+                            initY: pointer.lastY,
+                            transformation,
+                            other: {
+                                id: e.pointerId,
+                                initX: e.clientX,
+                                initY: e.clientY,
+                                lastX: e.clientX,
+                                lastY: e.clientY
+                            }
+                        })
+                        e.currentTarget.setPointerCapture(e.pointerId)
+                    }
+                } else {
+                    setPointer({
+                        id: e.pointerId,
+                        initX: e.clientX,
+                        initY: e.clientY,
+                        lastX: e.clientX,
+                        lastY: e.clientY,
+                        transformation,
+                        other: null
+                    })
+                    e.currentTarget.setPointerCapture(e.pointerId)
+                }
+            }}
+            onPointerMove={(e) => {
+                if (pointer?.id === e.pointerId || pointer?.other?.id === e.pointerId) {
+                    if (pointer.id === e.pointerId) {
+                        pointer.lastX = e.clientX;
+                        pointer.lastY = e.clientY;
+                    } else if (pointer.other) {
+                        pointer.other.lastX = e.clientX;
+                        pointer.other.lastY = e.clientY;
+                    }
+                    if (pointer.other) {
+                        const rect = e.currentTarget.getBoundingClientRect();
+                        const initXDiff = pointer.initX - pointer.other.initX;
+                        const initYDiff = pointer.initY - pointer.other.initY;
+                        const currentXDiff = pointer.lastX - pointer.other.lastX;
+                        const currentYDiff = pointer.lastY - pointer.other.lastY;
+                        // Difference between the angles of the initial and current line
+                        // between the pointers.
+                        const angleDiff =
+                            Math.atan2(currentYDiff, currentXDiff) -
+                            Math.atan2(initYDiff, initXDiff);
+                        // Distance between the pointers relative to their initial distance.
+                        const scale =
+                            Math.hypot(currentXDiff, currentYDiff) /
+                            Math.hypot(initXDiff, initYDiff);
+                        // Midpoint between the two pointers.
+                        const midX = (pointer.lastX + pointer.other.lastX) / 2;
+                        const midY = (pointer.lastY + pointer.other.lastY) / 2;
+                        // Offset of current midpoint from initial midpoint.
+                        const deltaX = midX - (pointer.initX + pointer.other.initX) / 2;
+                        const deltaY = midY - (pointer.initY + pointer.other.initY) / 2;
+                        // Centre of rotation/dilation
+                        const centreX = midX - (rect.left + rect.width / 2);
+                        const centreY = midY - (rect.top + rect.height / 2);
+                        setTransformation(multiply(
+                            // Rotate and scale around midpoint
+                            translate(centreX, centreY),
+                            rotate(angleDiff),
+                            dilate(scale),
+                            translate(-centreX, -centreY),
+                            translate(deltaX, deltaY),
+                            pointer.transformation!
+                        ));
+                    } else {
+                        setTransformation(multiply(
+                            translate(e.clientX - pointer.initX, e.clientY - pointer.initY),
+                            pointer.transformation!
+                        ))
+                    }
+                    mapRef.current!.style.transform = toCss(transformation);
+                }
+            }}
+            onPointerUp={pointerEnd}
+            onPointerCancel={pointerEnd}
+            onWheel={(e) => {
+                const rect = e.currentTarget.getBoundingClientRect();
+                const centreX = e.clientX - (rect.left + rect.width / 2);
+                const centreY = e.clientY - (rect.top + rect.height / 2);
+                setTransformation(multiply(
+                    translate(centreX, centreY),
+                    // Thanks Roger!
+                    dilate(1.001 ** -e.deltaY),
+                    translate(-centreX, -centreY),
+                    transformation
+                ))
+                mapRef.current!.style.transform = toCss(transformation);
+            }}
+        >
+            <img
+                src={GunnMapImage}
+                ref={mapRef}
+                draggable={false}
+                alt="Gunn map"
+                className="map-image"
+                style={{
+                    transform: toCss(transformation),
+                    filter: userData.options.theme === "dark" ? 'invert(1)' : ''
+                }}
+            />
+        </div>
+    )
+}
+
+type MatrixRow = [number, number, number];
+type Matrix = [MatrixRow, MatrixRow, MatrixRow];
+
+const identity = (): Matrix => [
+    [1, 0, 0],
+    [0, 1, 0],
+    [0, 0, 1]
+]
+const translate = (x: number, y: number): Matrix => [
+    [1, 0, x],
+    [0, 1, y],
+    [0, 0, 1]
+]
+const rotate = (angle: number): Matrix => [
+    [Math.cos(angle), -Math.sin(angle), 0],
+    [Math.sin(angle), Math.cos(angle), 0],
+    [0, 0, 1]
+]
+const dilate = (scaleFactor: number): Matrix => [
+    [scaleFactor, 0, 0],
+    [0, scaleFactor, 0],
+    [0, 0, 1]
+]
+
+const toCss = ([[a, c, x], [b, d, y]]: Matrix) => `matrix(${a}, ${b}, ${c}, ${d}, ${x}, ${y})`;
+
+function multiply(matrix1: Matrix, ...matrices: Matrix[]): Matrix {
+    if (matrices.length === 0) {
+        return matrix1;
+    }
+    const matrix2 = multiply(matrices[0], ...matrices.slice(1));
+    if (matrix1[0].length !== matrix2.length) {
+        throw new Error(
+            `Left operand's row count (${matrix1[0].length}) is not equal to the right operand's column count (${matrix2.length}).`
+        )
+    }
+    const product = Array.from(
+        { length: matrix1.length },
+        () => new Array(matrix2[0].length)
+    ) as Matrix;
+
+    for (let row = 0; row < matrix1.length; row++) {
+        for (let col = 0; col < matrix2[0].length; col++) {
+            let sum = 0;
+            for (let i = 0; i < matrix2.length; i++) {
+                sum += matrix1[row][i] * matrix2[i][col];
+            }
+            product[row][col] = sum;
+        }
+    }
+    return product;
+}
+
+export default ImageMap;
--- client/src/components/utilities/Map.tsx
+++ client/src/components/utilities/Map.tsx
@@ -1,112 +1,12 @@
 import React, { useEffect, useState, useRef, useContext } from 'react';
 import ReactDOM from 'react-dom';
+import ImageMap from './ImageMap';
 
-import GunnMapImage from '../../assets/gunnmap.png';
-import UserDataContext from '../../contexts/UserDataContext';
-
-// Pay attention in 'nal kids
-class Matrix {
-    private readonly arr: number[][];
-    private readonly r: number;
-    private readonly c: number;
-    constructor(arr: number[][]) {
-        this.arr = arr;
-        this.r = arr.length;
-        this.c = arr[0].length;
-    }
-
-    get(i: number,j: number) {
-        return this.arr[i][j];
-    }
-
-    set(i: number, j: number, v: number) {
-        this.arr[i][j] = v;
-    }
-
-    multiply(m: Matrix) {
-        if (this.c !== m.r) throw new Error('Incompatible Matrices!');
-
-        const R = this.r;
-        const C = m.c;
-        const A = this.c; // = m.r
-        let newMat = Array(R).fill(0).map(() => Array(C)); // create a R x C array
-
-        // uncool n^3 algo
-        for (let i = 0; i < R; i++) {
-            for (let j = 0; j < C; j++) {
-                let sum = 0;
-
-                for (let k = 0; k < A; k++) {
-                    sum += this.arr[i][k] * m.arr[k][j];
-                }
-
-                newMat[i][j] = sum;
-            }
-        }
-
-        return new Matrix(newMat);
-    }
-
-    getRaw() {
-        return this.arr.map(row => row.map(datum => datum));
-    }
-    copy() {
-        return new Matrix(this.getRaw());
-    }
-}
-
-class Transform {
-    public matrix: Matrix;
-    // public inv: Matrix;
-    constructor(a: number, b: number, c: number, d: number, x: number, y: number) {
-        this.matrix = new Matrix([[a, b, x], [c, d, y], [0, 0, 1]]);
-        // this.inv = this.matrix.inv();
-    }
- 
-    apply(x: number, y: number) {
-        let result = this.matrix.multiply(new Matrix([[x], [y], [1]])).getRaw();
-        return {
-            x: result[0],
-            y: result[1]
-        }
-    }
-
-    setTranslate(x:number,y: number) {
-        this.matrix.set(0, 2, x);
-        this.matrix.set(1, 2, y);
-    }
-
-    getTranslate() {
-        return {
-            x: this.matrix.get(0,2),
-            y: this.matrix.get(1,2)
-        }
-    }
-
-    dilate(factor: number) {
-        this.matrix = this.matrix.multiply(new Matrix([[factor, 0, 0], [0,factor,0], [0,0,1]]));
-    }
-
-    toString() {
-        return `matrix(${this.matrix.get(0, 0)}, ${this.matrix.get(0, 1)}, ${this.matrix.get(1, 0)}, ${this.matrix.get(1, 1)}, ${this.matrix.get(0, 2)}, ${this.matrix.get(1, 2)})`
-    }
-
-    copy() {
-        let c = new Transform(1,0,0,1,0,0);
-        c.matrix = this.matrix.copy();
-        return c;
-    }
-}
 
 const Map = () => {
     const [map, setMap] = useState<JSX.Element | null>(null);
-    let pos = (new Transform(1, 0, 0, 1, window.innerWidth/2, window.innerHeight/2));
-    let dragging: {simpleDrag: boolean, singleClick?: boolean, sx:number, sy:number, t: Transform, sx2?: number, sy2?: number}|null = (null);
 
     // const [gunnMap, setGunnMap] = useState<JSX.Element | null>(null)
-    const mapRef = useRef<null|HTMLImageElement>(null);
-
-    const userData = useContext(UserDataContext);
 
     // Render the portal in the useEffect to guarantee that all elements have been rendered into the DOM and
     // document.getElementById('content') is not null
@@ -127,91 +27,9 @@ const Map = () => {
     // )), [])
 
     useEffect(() => setMap(ReactDOM.createPortal(
-        <img
-            src={GunnMapImage}
-            ref={mapRef}
-            draggable={false}
-            alt=""
-            style={{
-                position: "fixed",
-                width: '1000px',
-                transform: `${pos.toString()}`,
-                left: 0,
-                top: 100,
-                maxHeight: '100vh',
-                maxWidth: '100%',
-                filter: userData.options.theme === "dark" ? 'invert(1)' : ''
-            }}
-            onMouseDown={(e) => {
-                dragging = ({ simpleDrag: true, sx: e.clientX, sy: e.clientY, t: pos.copy() });
-            }}
-            onMouseMove={(e) => {
-                if (dragging && dragging.simpleDrag) {
-                    (pos.setTranslate(e.clientX - dragging.sx + dragging.t.getTranslate().x, e.clientY - dragging.sy + dragging.t.getTranslate().y));
-                    e.currentTarget.style.transform = `${pos.toString()}`;
-                }
-            }}
-            onMouseUp={() => {
-                dragging = null
-            }}
-            onWheel={(e) => {
-                //e.preventDefault();
-
-                pos.dilate(Math.pow(1.002, -e.deltaY))
-                e.currentTarget.style.transform = `${pos.toString()}`;
-            }}
-            onTouchStart={(e) => {
-                if (dragging) {
-                    dragging.simpleDrag = false;
-                    dragging.sx = e.touches[0].clientX;
-                    dragging.sy = e.touches[0].clientY;
-                    dragging.sx2 = e.touches[1].clientX;
-                    dragging.sy2 = e.touches[1].clientY;
-                    dragging.t = pos.copy();
-                } else {
-                    dragging = ({ simpleDrag: true, sx: e.touches[0].clientX, sy: e.touches[0].clientY, t: pos.copy() });
-                }
-            }}
-            onTouchMove={(e) => {
-                //e.preventDefault();
-
-                if (dragging && dragging.simpleDrag) {
-                    (pos.setTranslate(e.touches[0].clientX - dragging.sx + dragging.t.getTranslate().x, e.touches[0].clientY - dragging.sy + dragging.t.getTranslate().y));
-                    e.currentTarget.style.transform = `${pos.toString()}`;
-                }
-
-                if (dragging && !dragging.simpleDrag && dragging.sx2 && dragging.sy2) {
-                    // ruh roh
-                    // have to find some way to map from (sx,sy) and (sx2,sy2) to (nx,ny) and (nx2, ny2) respectively using an affine transformation
-                    const { sx, sy, sx2, sy2 } = dragging;
-                    const [nx, ny, nx2, ny2] = [e.touches[0].clientX, e.touches[0].clientY, e.touches[1].clientX, e.touches[1].clientY];
-
-                    const deg = Math.atan2(ny2 - ny, nx2 - nx) - Math.atan2(sy2 - sy, sx2 - sx);
-                    const scale = Math.sqrt((nx2 - nx) ** 2 + (ny2 - ny) ** 2) / Math.sqrt((sx2 - sx) ** 2 + (sy2 - sy) ** 2)
-                    const trans = new Transform(1, 0, 0, 1, nx, ny);
-                    const ntrans = new Transform(1, 0, 0, 1, -sx, -sy);
-                    const rotandscale = new Transform(scale * Math.cos(deg), -scale * Math.sin(deg), scale * Math.sin(deg), scale * Math.cos(deg), 0, 0);
-
-                    // math
-                    // first apply previous transform
-                    // then apply the affine transformation
-                    pos.matrix = trans.matrix.multiply(rotandscale.matrix).multiply(ntrans.matrix).multiply(dragging.t.matrix);
-                    e.currentTarget.style.transform = `${pos.toString()}`;
-                }
-            }}
-            onTouchEnd={() => {
-                dragging = null;
-            }}
-        />,
+        <ImageMap/>,
         document.getElementById('content')!
     )), [])
-
-    useEffect(() => {
-        // Unideal, but https://github.com/facebook/react/issues/14856 forces this
-        // onWheel and onTouchMove are both passive event listeners so preventDefault does not work within them
-        mapRef.current?.addEventListener('wheel', (e) => e.preventDefault());
-        mapRef.current?.addEventListener('touchmove', (e) => e.preventDefault());
-    }, [mapRef])
 
 
     return (
