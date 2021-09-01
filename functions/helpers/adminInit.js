e7732389ba96d6c731adea545e5335428c9a6bd7
Roger Fan <rogerjoeyfan@gmail.com>
2021-08-31 21:09:00 -0700

    Testing out if credentials are needed for initializing firebase SDK

--- functions/helpers/adminInit.js
+++ functions/helpers/adminInit.js
@@ -1,7 +1,6 @@
 const admin = require("firebase-admin")
-const serviceAccount = require("../credentials/firebaseAdmin.json")
-admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-})
+// const serviceAccount = require("../credentials/firebaseAdmin.json")
+admin.initializeApp()
+    // credential: admin.credential.cert(serviceAccount)
 
 module.exports = admin
