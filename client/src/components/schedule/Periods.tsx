7cad0d17b6d9f6930819d1ee98373f23751347dc
Kevin Yu <kevinyu567@gmail.com>
2021-10-25 10:15:47 -0700

    Update dark mode default colors

--- client/src/components/schedule/Periods.tsx
+++ client/src/components/schedule/Periods.tsx
@@ -131,8 +131,8 @@ export const darkPerColors =
     //periodColors.map(x => darken(x))
     //['#ef5350', '#14B8A6', '#42a5f5', '#7e57c2',
     //    '#ef6c00', '#7cb342', '#FBBF24', '#3F3F46'];
-    ['#eb4747', '#29a395', '#2b98ca', '#814eda',
-        '#e77727', '#7cb342', '#eeb82f', '#373739']
+    ['#eb4747', '#29a395', '#2b98ca', '#7842d7',
+        '#d22d51', '#ed7621', '#eeb82f', '#373739']
 
 
 // Turns day of the week into schedule object key; Thursday is R, Saturday is A
