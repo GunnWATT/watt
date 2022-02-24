8b6a4cfa056c66a6b715a84e41979d87dfd7eac0
Kevin Yu <kevinyu567@gmail.com>
2022-02-24 15:30:44 -0800

    Event description disclosure logic

--- client/src/components/schedule/Event.tsx
+++ client/src/components/schedule/Event.tsx
@@ -27,10 +27,19 @@ export default function Event(props: GCalEvent) {
         return "Unknown Date";
     }
 
+    // Don't render a disclosure if there's no event description
+    if (!description) return (
+        <li className="w-full text-left px-4 py-2 rounded bg-[color:var(--content-secondary)] text-sm bg-opacity-50">
+            <strong>{summary}</strong>
+            <p className="secondary">{formatDateTime(start, end)}</p>
+            {location && <p className="secondary">@ {location}</p>}
+        </li>
+    )
+
     return (
         <Disclosure as={'li'}>
             {({open}) => (<>
-                <Disclosure.Button className="flex items-center justify-between gap-2 w-full text-left px-4 py-2 rounded bg-[color:var(--content-secondary)] text-sm bg-opacity-50" key={summary}>
+                <Disclosure.Button className="flex items-center justify-between gap-2 w-full text-left px-4 py-2 rounded bg-[color:var(--content-secondary)] text-sm bg-opacity-50">
                     <div>
                         <strong>{summary}</strong>
                         <p className="secondary">{formatDateTime(start, end)}</p>
