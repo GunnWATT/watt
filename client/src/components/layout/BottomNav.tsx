c7d05be2af26806872c8a7cd9e1e4790ef8c577f
Kevin Yu <kevinyu567@gmail.com>
2022-06-05 23:00:43 -0700

    Tailwindify `BottomNav`

--- client/src/components/layout/BottomNav.tsx
+++ client/src/components/layout/BottomNav.tsx
@@ -3,7 +3,8 @@ import {useAuth, useSigninCheck} from 'reactfire';
 
 // Components
 import SidebarItem from './SidebarItem';
-import GoogleSignInBtn from "../firebase/GoogleSignInBtn"
+import AnimatedPopover from './AnimatedPopover';
+import GoogleSignInBtn from '../firebase/GoogleSignInBtn';
 import GoogleSignOutBtn from '../firebase/GoogleSignOutBtn';
 
 // Icons
@@ -15,7 +16,7 @@ export default function BottomNav() {
     const {status, data: signInCheckResult} = useSigninCheck();
 
     return (
-        <footer className="bottom-nav z-10 text-lg bg-sidebar dark:bg-sidebar-dark fixed bottom-0 left-0 w-screen">
+        <footer className="z-10 text-lg bg-sidebar dark:bg-sidebar-dark fixed bottom-0 left-0 w-screen">
             <nav className="flex justify-between py-3 px-8 max-w-lg mx-auto">
                 {/* Nav */}
                 <SidebarItem to="/clubs" icon={Users} />
@@ -24,14 +25,14 @@ export default function BottomNav() {
                 <SidebarItem to="/utilities" icon={Tool} />
 
                 {/* Dropup */}
-                <Popover className="item dropup-wrapper relative flex">
+                <Popover className="item relative flex">
                     {({open}) => (<>
                         <Popover.Button className="p-2">
                             {open ? <ChevronDown /> : <ChevronUp />}
                         </Popover.Button>
 
-                        {/* TODO: add transitions? */}
-                        <Popover.Panel className="bottom-nav-dropup absolute flex flex-col gap-4 bg-sidebar dark:bg-sidebar-dark shadow-lg rounded-xl px-1 py-3">
+                        {/* TODO: this can use a custom transition where it translates up instead of just `AnimatedPopover` */}
+                        <AnimatedPopover className="absolute left-1/2 -top-7 -translate-x-1/2 -translate-y-full flex flex-col gap-4 bg-sidebar dark:bg-sidebar-dark shadow-lg rounded-xl px-1 py-3">
                             {/* Sign In / Out */}
                             {signInCheckResult?.signedIn ? (
                                 <GoogleSignOutBtn mobile />
@@ -40,7 +41,7 @@ export default function BottomNav() {
                             )}
 
                             <SidebarItem to="/settings" icon={Settings} />
-                        </Popover.Panel>
+                        </AnimatedPopover>
                     </>)}
                 </Popover>
             </nav>
