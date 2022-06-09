01dfe268d1768158b0a39419b3184681cfe65b11
Kevin Yu <kevinyu567@gmail.com>
2022-06-09 15:48:32 -0700

    Tailwindify quick week cal, fix excessive rerenders

--- client/src/components/classes/DashboardQuickWeekCal.tsx
+++ client/src/components/classes/DashboardQuickWeekCal.tsx
@@ -1,4 +1,4 @@
-import {useContext} from 'react';
+import {useContext, useMemo} from 'react';
 import {DateTime} from 'luxon';
 
 // Contexts
@@ -22,7 +22,7 @@ function DashboardQuickCalDot(props: DashboardQuickCalDotProps) {
     const userData = useContext(UserDataContext);
     return (
         <div
-            className="quick-calendar-dot"
+            className="w-2.5 h-2.5 rounded-full m-[3px]"
             style={{
                 backgroundColor: props.completed ? 'var(--content-primary)' : parsePeriodColor(props.course, userData),
                 border: props.completed ? '2px inset var(--secondary)' : ''
@@ -43,14 +43,19 @@ function DashboardQuickCalDay(props: DashboardQuickCalDayProps) {
     const relevantAssignments = upcoming.filter((a) => a.timestamp!.ordinal === day.ordinal);
     const active = selected === 'A' ? !!(getSchedule(day, alternates).periods) : !!hasClass(day, selected, alternates);
 
-    // Lots of flexbox
     return (
-        <div className={"quick-calendar-day" + (active ? '-active' : '-inactive')}>
-            <div className="quick-calendar-day-num">
+        <div className={'p-1 rounded min-h-[70px] text-center ' + (active ? 'bg-background dark:bg-content-dark' : 'bg-content-secondary dark:bg-background-dark')}>
+            <div>
                 {weekdays[day.weekday % 7]}{screenType !== 'phone' && ` • ${day.day}`}
             </div>
-            <div className="quick-calendar-dots">
-                {relevantAssignments.map((a, i) => <DashboardQuickCalDot key={a.period + i} course={a.period} completed={a.completed} />)}
+            <div className="flex flex-wrap justify-center my-0.5">
+                {relevantAssignments.map((a, i) => (
+                    <DashboardQuickCalDot
+                        key={a.period + i}
+                        course={a.period}
+                        completed={a.completed}
+                    />
+                ))}
             </div>
         </div>
     );
@@ -61,15 +66,24 @@ export default function DashboardQuickWeekCal(props: DashboardQuickWeekCalProps)
     const screenType = useScreenType();
     const numDaysShown = screenType === 'phone' ? 8 : 7;
 
-    let mutableTime = useContext(CurrentTimeContext);
-    const days = [];
-    for (let i = 0; i < numDaysShown; i++) { // Add 1 for each day of the week
-        days.push(mutableTime);
-        mutableTime = mutableTime.plus({day: 1});
-    }
+    const time = useContext(CurrentTimeContext);
+
+    // Generate the days to display in the calendar from the current date and number of days to show,
+    // memoizing the value to prevent unnecessary rerenders.
+    // TODO: is this the best way of doing this? Also, test to make sure `time.day` and `time.weekday` work
+    // for the deps array
+    const days = useMemo(() => {
+        let mutableTime = time;
+        const days = [];
+        for (let i = 0; i < numDaysShown; i++) { // Add 1 for each day of the week
+            days.push(mutableTime);
+            mutableTime = mutableTime.plus({day: 1});
+        }
+        return days;
+    }, [numDaysShown, time.day, time.weekday]);
 
     return (
-        <div className={"quick-calendar " + screenType}>
+        <div className="grid grid-cols-4 md:grid-cols-7 gap-2 mt-2.5 mb-4">
             {days.map((day) => (
                 <DashboardQuickCalDay
                     key={day.toISO()}
