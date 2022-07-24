4a322046b52fea9d4d6e6ea18cc7ab17e561e64c
Kevin Yu <kevinyu567@gmail.com>
2022-07-24 15:00:06 -0700

    Use `vite-plugin-pwa`

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
 
 export default defineConfig({
-    plugins: [react()],
+    plugins: [
+        react(),
+        VitePWA({registerType: 'autoUpdate'})
+    ],
     server: {
         port: 3000
     }
