b5176677d5ea6195fb6840f11cb2fb4cf63eff3f
Kevin Yu <kevinyu567@gmail.com>
2022-07-25 17:08:52 -0700

    Unregister service worker on `/api`, fix JSX runtime

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,8 +5,19 @@ import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
     plugins: [
-        react(),
-        VitePWA({registerType: 'autoUpdate'})
+        react({
+            // Temporarily revert to `React.createElement` JSX transform due to scoping issue with `headlessui`.
+            // https://github.com/tailwindlabs/headlessui/issues/1693
+            jsxRuntime: 'classic'
+        }),
+        VitePWA({
+            registerType: 'autoUpdate',
+            workbox: {
+                // Disable service worker on `/api`, allowing the client to request the api without receiving a
+                // cached 404 page.
+                navigateFallbackDenylist: [/^\/api/]
+            }
+        })
     ],
     build: {
         sourcemap: true
