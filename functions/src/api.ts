784941dbcf7a8d725c79e5601d8e4958107322eb
Kevin Yu <kevinyu567@gmail.com>
2022-08-02 19:20:24 -0700

    Fix alternates parsing in API

--- functions/src/api.ts
+++ functions/src/api.ts
@@ -9,6 +9,7 @@ import {getSchedule} from '@watt/shared/util/schedule';
 import clubs from '@watt/shared/data/clubs';
 import staff from '@watt/shared/data/staff';
 import {getNextPeriod, getNextPeriodMessage} from './util/schedule';
+import {Alternates} from '@watt/client/src/contexts/AlternatesContext';
 
 const app = express();
 app.use(cors({origin: true}));
@@ -16,7 +17,7 @@ app.use(cors({origin: true}));
 
 async function getAlternates() {
     const doc = await admin.firestore().collection('gunn').doc('alternates').get();
-    return doc.data();
+    return doc.data() as Alternates | undefined;
 }
 
 // GET /api/clubs
@@ -43,8 +44,8 @@ app.get('/api/alternates', async (req, res) => {
 // with `periods` set to an array of the day's periods (or `null` if there is no school) and `alternate` set to whether
 // the returned schedule is an alternate.
 app.get('/api/schedule', async (req, res) => {
-    const alternates = await getAlternates();
-    if (!alternates) return res.status(500).json({error: 'Alternates document malformed or nonexistant.'});
+    const data = await getAlternates();
+    if (!data) return res.status(500).json({error: 'Alternates document malformed or nonexistant.'});
 
     if (req.query.date) {
         if (typeof req.query.date !== 'string')
@@ -54,17 +55,17 @@ app.get('/api/schedule', async (req, res) => {
         if (!date.isValid)
             return res.status(400).json({error: `Error parsing date string: ${date.invalidExplanation}.`});
 
-        return res.json(getSchedule(date, alternates));
+        return res.json(getSchedule(date, data.alternates));
     }
 
-    const schedule = getSchedule(DateTime.now(), alternates);
+    const schedule = getSchedule(DateTime.now(), data.alternates);
     return res.json(schedule);
 });
 
 // GET /api/next-period
 app.get('/api/next-period', async (req, res) => {
-    const alternates = await getAlternates();
-    if (!alternates) return res.status(500).json({error: 'Alternates document malformed or nonexistant.'});
+    const data = await getAlternates();
+    if (!data) return res.status(500).json({error: 'Alternates document malformed or nonexistant.'});
 
     if (req.query.date) {
         if (typeof req.query.date !== 'string')
@@ -74,17 +75,17 @@ app.get('/api/next-period', async (req, res) => {
         if (!date.isValid)
             return res.status(400).json({error: `Error parsing date string: ${date.invalidExplanation}.`});
 
-        return res.json(getNextPeriod(date, alternates));
+        return res.json(getNextPeriod(date, data.alternates));
     }
 
-    const next = getNextPeriod(DateTime.now(), alternates);
+    const next = getNextPeriod(DateTime.now(), data.alternates);
     return res.json(next);
 });
 
 // GET /api/next-period-message
 app.get('/api/next-period-message', async (req, res) => {
-    const alternates = await getAlternates();
-    if (!alternates) return res.status(500).json({error: 'Alternates document malformed or nonexistant.'});
+    const data = await getAlternates();
+    if (!data) return res.status(500).json({error: 'Alternates document malformed or nonexistant.'});
 
     if (req.query.date) {
         if (typeof req.query.date !== 'string')
@@ -94,10 +95,10 @@ app.get('/api/next-period-message', async (req, res) => {
         if (!date.isValid)
             return res.status(400).json({error: `Error parsing date string: ${date.invalidExplanation}.`});
 
-        return res.json({message: getNextPeriodMessage(date, alternates)});
+        return res.json({message: getNextPeriodMessage(date, data.alternates)});
     }
 
-    const message = getNextPeriodMessage(DateTime.now(), alternates);
+    const message = getNextPeriodMessage(DateTime.now(), data.alternates);
     return res.json({message});
 });
 
