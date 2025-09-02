f1e75513daa84fda918bec9e8531bf4574a48b9d
Eason Lu <aelnosu@gmail.com>
2025-09-01 20:43:42 -0700

    Update NYTimes component to reflect changes in student access and registration process

--- client/src/components/resources/NYTimes.tsx
+++ client/src/components/resources/NYTimes.tsx
@@ -6,11 +6,10 @@ export default function NYTimes() {
             </h1>
 
             <p className="mb-4">
-                All PAUSD students have free access to a New York Times subscription. Register at the link below;
-                when prompted, log in with your PAUSD email. Subscriptions have to be renewed every year.
+                Gunn students no longer have access to the New York Times through the school. However students can still obtain free access from the Palo Alto City Library using their student ID as a library card. See the library card information down below.
             </p>
-            <a href="https://myaccount.nytimes.com/verification/edupass" target="_blank" rel="noopener noreferrer">
-                Register for an educational New York Times subscription →
+            <a href="https://library.cityofpaloalto.org/resources-types/magazine/" target="_blank" rel="noopener noreferrer">
+                Register for an New York Times subscription (Scoll down until you see the <i>New York Times Online</i> option)→
             </a>
         </>
     )
