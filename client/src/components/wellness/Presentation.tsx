3782019e6d3d9cab3d624bced175ab7de2f51e0b
Eason Lu <aelnosu@gmail.com>
2025-09-03 17:20:52 -0700

    Replace SafetyPresentation image with multiple Wellness presentation slides
    It just take too long to load one big image. Split it into multiple smaller images. Also used webp format for better compression.

--- client/src/components/wellness/Presentation.tsx
+++ client/src/components/wellness/Presentation.tsx
@@ -1,9 +1,42 @@
-import SafetyPresentation from '../../assets/Wellness-Safety.png';
+import SafetyPresentation1 from '../../assets/Wellness/Wellness_1.webp';
+import SafetyPresentation2 from '../../assets/Wellness/Wellness_2.webp';
+import SafetyPresentation3 from '../../assets/Wellness/Wellness_3.webp';
+import SafetyPresentation4 from '../../assets/Wellness/Wellness_4.webp';
+import SafetyPresentation5 from '../../assets/Wellness/Wellness_5.webp';
+import SafetyPresentation6 from '../../assets/Wellness/Wellness_6.webp';
+import SafetyPresentation7 from '../../assets/Wellness/Wellness_7.webp';
+import SafetyPresentation8 from '../../assets/Wellness/Wellness_8.webp';
+import SafetyPresentation9 from '../../assets/Wellness/Wellness_9.webp';
+import SafetyPresentation10 from '../../assets/Wellness/Wellness_10.webp';
+import SafetyPresentation11 from '../../assets/Wellness/Wellness_11.webp';
+import SafetyPresentation12 from '../../assets/Wellness/Wellness_12.webp';
+import SafetyPresentation13 from '../../assets/Wellness/Wellness_13.webp';
+import SafetyPresentation14 from '../../assets/Wellness/Wellness_14.webp';
+import SafetyPresentation15 from '../../assets/Wellness/Wellness_15.webp';
+import SafetyPresentation16 from '../../assets/Wellness/Wellness_16.webp';
+import SafetyPresentation17 from '../../assets/Wellness/Wellness_17.webp';
+
 
 export default function Presentation() {
     return (
         <>
-            <img src={SafetyPresentation} alt="Wellness Safety Presentation" className="max-w-full rounded-lg mb-4" />
+            <img src={SafetyPresentation1} alt="Wellness Safety Presentation Slide1" className="max-w-full rounded-lg mb-0" />
+            <img src={SafetyPresentation2} alt="Wellness Safety Presentation Slide2" className="max-w-full rounded-lg mb-0" />
+            <img src={SafetyPresentation3} alt="Wellness Safety Presentation Slide3" className="max-w-full rounded-lg mb-0" />
+            <img src={SafetyPresentation4} alt="Wellness Safety Presentation Slide4" className="max-w-full rounded-lg mb-0" />
+            <img src={SafetyPresentation5} alt="Wellness Safety Presentation Slide5" className="max-w-full rounded-lg mb-0" />
+            <img src={SafetyPresentation6} alt="Wellness Safety Presentation Slide6" className="max-w-full rounded-lg mb-0" />
+            <img src={SafetyPresentation7} alt="Wellness Safety Presentation Slide7" className="max-w-full rounded-lg mb-0" />
+            <img src={SafetyPresentation8} alt="Wellness Safety Presentation Slide8" className="max-w-full rounded-lg mb-0" />
+            <img src={SafetyPresentation9} alt="Wellness Safety Presentation Slide9" className="max-w-full rounded-lg mb-0" />
+            <img src={SafetyPresentation10} alt="Wellness Safety Presentation Slide10" className="max-w-full rounded-lg mb-0" />
+            <img src={SafetyPresentation11} alt="Wellness Safety Presentation Slide11" className="max-w-full rounded-lg mb-0" />
+            <img src={SafetyPresentation12} alt="Wellness Safety Presentation Slide12" className="max-w-full rounded-lg mb-0" />
+            <img src={SafetyPresentation13} alt="Wellness Safety Presentation Slide13" className="max-w-full rounded-lg mb-0" />
+            <img src={SafetyPresentation14} alt="Wellness Safety Presentation Slide14" className="max-w-full rounded-lg mb-0" />
+            <img src={SafetyPresentation15} alt="Wellness Safety Presentation Slide15" className="max-w-full rounded-lg mb-0" />
+            <img src={SafetyPresentation16} alt="Wellness Safety Presentation Slide16" className="max-w-full rounded-lg mb-0" />
+            <img src={SafetyPresentation17} alt="Wellness Safety Presentation Slide17" className="max-w-full rounded-lg mb-0" />
         </>
     );
 }
